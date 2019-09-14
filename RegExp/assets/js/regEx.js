cc77f9da83556c43cc6f352fd3c06fdeb2748052
Marcello Alfaro <marcello.alfaro1@gmail.com>
2019-09-13 21:36:18 -0500

    completed regEx project

--- RegExp/assets/js/regEx.js
+++ RegExp/assets/js/regEx.js
@@ -6,7 +6,7 @@ var hashtagText;
 var variableText;
 var resultado;
 
-for (let i = 0; i < mode.length; i++) {
+for (var i = 0; i < mode.length; i++) {
   document.querySelector('#display1').style.display = 'inline-block';
   mode[i].addEventListener('click', function() {
     mode[0].classList.remove('selected');
@@ -17,22 +17,25 @@ for (let i = 0; i < mode.length; i++) {
       document.querySelector('#display1').style.display = 'inline-block';
       document.querySelector('#display2').style.display = 'none';
       document.querySelector('#display3').style.display = 'none';
+      document.querySelector('div.verificar h1').textContent = '';
     } else if (this.textContent == 'Variable') {
       document.querySelector('#display1').style.display = 'none';
       document.querySelector('#display2').style.display = 'inline-block';
       document.querySelector('#display3').style.display = 'none';
+      document.querySelector('div.verificar h1').textContent = '';
     } else {
       document.querySelector('#display1').style.display = 'none';
       document.querySelector('#display2').style.display = 'none';
       document.querySelector('#display3').style.display = 'inline-block';
+      document.querySelector('div.verificar h1').textContent = '';
     }
   });
 }
 
-//*? Codigo para Expresion regular de hashtags
+//*! Codigo para Expresion regular de hashtags
 btnVerificarHash.addEventListener('click', function() {
   hashtagText = document.querySelector('#hash').value;
-  const hashtagRegExp = new RegExp(/^([#][A-Z])(\w{1,15})$/);
+  const hashtagRegExp = new RegExp(/^([#][A-Z])(\w{1,13})$/);
   resultado = hashtagRegExp.test(hashtagText);
   if (resultado) {
     document.querySelector('div.verificar h1').textContent = 'Correcto!';
@@ -42,11 +45,11 @@ btnVerificarHash.addEventListener('click', function() {
     document.querySelector('div.verificar h1').style.color = '#e74c3c';
   }
 });
-/* +[0-9]{1,3} (([a-zA-Z]){1,8}) */
-//*? Codigo para Expresion regular de variables
+
+//*! Codigo para Expresion regular de variables
 btnVerificarVar.addEventListener('click', function() {
   variableText = document.querySelector('#variable').value;
-  const varRegExp = new RegExp(/^([A-Z][_])([a-zA-Z]{0,8}[0-9]?[0-9]?[0-9])$/);
+  const varRegExp = new RegExp(/^([A-Z][_])([a-zA-Z]{1,5}[0-9]{1,3})$/);
   resultado = varRegExp.test(variableText);
   if (resultado) {
     document.querySelector('div.verificar h1').textContent = 'Correcto!';
@@ -57,10 +60,12 @@ btnVerificarVar.addEventListener('click', function() {
   }
 });
 
-//*? Codigo para Expresion regular para contraseñas
+//*! Codigo para Expresion regular para contraseñas
 btnVerificarContr.addEventListener('click', function() {
-  variableText = document.querySelector('#variable').value;
-  const varRegExp = new RegExp(/^([#%&$]    [#%&$]){8,12}$/);
+  variableText = document.querySelector('#contraseña').value;
+  const varRegExp = new RegExp(
+    /^(?=.*[#%&$])(?=.*\d)(?=.*[A-Z])([#%&$])?([A-Za-z0-9]{7,11})([#%&$])?$/
+  );
   resultado = varRegExp.test(variableText);
   if (resultado) {
     document.querySelector('div.verificar h1').textContent = 'Correcto!';
